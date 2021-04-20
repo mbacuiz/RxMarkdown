3c0f1dfcd745736901bc9ecbb06dae0052b555f0
mbacuiz <59240607+mbacuiz@users.noreply.github.com>
2021-04-20 17:42:31 +0800

    Update Link.java

--- markdown-processor/src/main/java/com/yydcdut/markdown/config/Link.java
+++ markdown-processor/src/main/java/com/yydcdut/markdown/config/Link.java
@@ -24,7 +24,7 @@ import com.yydcdut.markdown.callback.OnLinkClickCallback;
  */
 public class Link {
     public int color = Color.RED;
-    public boolean autoLink = false;
+    public boolean autoLink = true;
     public boolean underline = true;
     public OnLinkClickCallback callback;
 }
